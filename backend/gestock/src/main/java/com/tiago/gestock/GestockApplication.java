83e25e6271d24f5cc0684e8c3925f7d095178bce
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-11 13:03:39 -0300

    initial commit

---
+++ backend/gestock/src/main/java/com/tiago/gestock/GestockApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.gestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestockApplication.class, args);
+	}
+
+}
---
+++ backend/gestock/src/test/java/com/tiago/gestock/GestockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.gestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
