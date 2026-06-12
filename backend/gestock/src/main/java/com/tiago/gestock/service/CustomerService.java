b76e62e02f7cba43d3782af3cf35615bfcdee433
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-12 16:14:11 -0300

    feat(customer-service): add create method

--- backend/gestock/src/main/java/com/tiago/gestock/service/CustomerService.java
+++ backend/gestock/src/main/java/com/tiago/gestock/service/CustomerService.java
@@ -3,11 +3,13 @@ package com.tiago.gestock.service;
 
 import com.tiago.gestock.mapper.CustomerMapper;
 import com.tiago.gestock.model.Customer;
+import com.tiago.gestock.model.dto.CustomerRequestDTO;
 import com.tiago.gestock.model.dto.CustomerResponseDTO;
 import com.tiago.gestock.repository.CustomerRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CustomerService {
@@ -18,8 +20,16 @@ public class CustomerService {
     @Autowired
     private CustomerMapper mapper;
     
+    @Transactional(readOnly = true)
     public List<CustomerResponseDTO> findAll(){
         List<Customer> customers = this.repository.findAll();
         return this.mapper.toResponseDTOList(customers);
+    }
+    
+    @Transactional
+    public CustomerResponseDTO create(CustomerRequestDTO request){
+        Customer customer = this.mapper.toEntity(request);
+        this.repository.save(customer);
+        return this.mapper.toResponseDTO(customer);
     }
 }
