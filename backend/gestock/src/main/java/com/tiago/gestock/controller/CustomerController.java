f59a85a95515a92418421a06dae217146f371c04
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-12 16:37:43 -0300

    feat(customer-controller): add findById method

--- backend/gestock/src/main/java/com/tiago/gestock/controller/CustomerController.java
+++ backend/gestock/src/main/java/com/tiago/gestock/controller/CustomerController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,14 @@ public class CustomerController {
     @GetMapping
     public ResponseEntity<List<CustomerResponseDTO>> findAll(){
         return ResponseEntity.ok(this.service.findAll());
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerResponseDTO> findById(@PathVariable Long id){
+        if(id < 1){
+            throw new RuntimeException("O id deve ser maior que zero.");
+        }
+        return ResponseEntity.ok(this.service.findById(id));
     }
     
     @PostMapping
