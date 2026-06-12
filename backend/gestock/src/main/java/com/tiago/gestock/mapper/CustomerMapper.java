be8f36534552b9adad1482435ca8f67c9b947c2a
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-12 17:05:01 -0300

    feat: add updateEntityFromDTO mapper

--- backend/gestock/src/main/java/com/tiago/gestock/mapper/CustomerMapper.java
+++ backend/gestock/src/main/java/com/tiago/gestock/mapper/CustomerMapper.java
@@ -5,6 +5,7 @@ import com.tiago.gestock.model.dto.CustomerRequestDTO;
 import com.tiago.gestock.model.dto.CustomerResponseDTO;
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -15,4 +16,6 @@ public interface CustomerMapper {
     CustomerResponseDTO toResponseDTO(Customer entity);
     
     List<CustomerResponseDTO> toResponseDTOList(List<Customer> entityList);
+    
+    void updateEntityFromDTO(CustomerRequestDTO dto, @MappingTarget Customer entity);
 }
