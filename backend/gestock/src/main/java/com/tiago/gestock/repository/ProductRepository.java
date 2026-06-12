63d906d8bdd5df638ec91565810175970dccd49e
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-12 13:36:39 -0300

    feat: create Product repository

---
+++ backend/gestock/src/main/java/com/tiago/gestock/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+
+package com.tiago.gestock.repository;
+
+import com.tiago.gestock.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{}
