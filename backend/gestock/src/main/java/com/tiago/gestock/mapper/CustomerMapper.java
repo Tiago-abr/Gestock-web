1cf09b14fcc7329c1fed9868a68bc468de314d26
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-12 15:53:15 -0300

    chore: fix mapper syntax

---
+++ backend/gestock/src/main/java/com/tiago/gestock/mapper/CustomerMapper.java
@@ -0,0 +1,18 @@
+package com.tiago.gestock.mapper;
+
+import com.tiago.gestock.model.Customer;
+import com.tiago.gestock.model.dto.CustomerRequestDTO;
+import com.tiago.gestock.model.dto.CustomerResponseDTO;
+import java.util.List;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CustomerMapper {
+        
+    Customer toEntity(CustomerRequestDTO dto);
+    
+    CustomerResponseDTO toResponseDTO(Customer entity);
+    
+    List<CustomerResponseDTO> toResponseDTOList(List<Customer> entityList);
+}
--- backend/gestock/src/main/java/com/tiago/gestock/mapper/SaleMapper.java
+++ backend/gestock/src/main/java/com/tiago/gestock/mapper/SaleMapper.java
@@ -4,8 +4,9 @@ import com.tiago.gestock.model.Sale;
 import com.tiago.gestock.model.dto.SaleResponseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface SaleMapper {
     
     @Mapping(source = "customer.name", target = "customerName")
