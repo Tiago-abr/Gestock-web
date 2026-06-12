e56d20cbff0b3f83d2de4a545ca99618fb8defa4
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-12 13:36:05 -0300

    feat: create Customer repository

---
+++ backend/gestock/src/main/java/com/tiago/gestock/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.tiago.gestock.repository;
+
+import com.tiago.gestock.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long>{}
