99ff07b34d0821a17049204a4176d2b62a7f01d2
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-13 18:51:00 -0300

    feat(sale-controller): add update method

--- backend/gestock/src/main/java/com/tiago/gestock/controller/SaleController.java
+++ backend/gestock/src/main/java/com/tiago/gestock/controller/SaleController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +41,10 @@ public class SaleController {
 				.buildAndExpand(response.id()).toUri();
         
         return ResponseEntity.created(uri).body(response);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<SaleResponseDTO> update(@PathVariable Long id, @RequestBody SaleRequestDTO request){
+        return ResponseEntity.ok(this.service.update(id, request));
     }
 }
