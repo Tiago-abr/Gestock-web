4457e7f2754c476b2c7442bbda1e6a878a92c9d9
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-13 18:50:36 -0300

    feat(sale-service): add update method

--- backend/gestock/src/main/java/com/tiago/gestock/service/SaleService.java
+++ backend/gestock/src/main/java/com/tiago/gestock/service/SaleService.java
@@ -1,13 +1,17 @@
 package com.tiago.gestock.service;
 
 import com.tiago.gestock.mapper.SaleMapper;
+import com.tiago.gestock.model.Customer;
 import com.tiago.gestock.model.Product;
 import com.tiago.gestock.model.Sale;
 import com.tiago.gestock.model.SaleItem;
+import com.tiago.gestock.model.User;
 import com.tiago.gestock.model.dto.SaleRequestDTO;
 import com.tiago.gestock.model.dto.SaleResponseDTO;
+import com.tiago.gestock.repository.CustomerRepository;
 import com.tiago.gestock.repository.ProductRepository;
 import com.tiago.gestock.repository.SaleRepository;
+import com.tiago.gestock.repository.UserRepository;
 import java.math.BigDecimal;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +25,13 @@ public class SaleService {
     private SaleRepository repository;
     
     @Autowired
-    private ProductRepository ProductRepository;
+    private ProductRepository productRepository;
+    
+    @Autowired 
+    private CustomerRepository customerRepository;
+    
+    @Autowired
+    private UserRepository userRepository;
     
     @Autowired
     private SaleMapper mapper;
@@ -45,7 +55,7 @@ public class SaleService {
        Sale sale = this.mapper.toEntity(request);
        
        for(SaleItem item : sale.getItems()){
-           Product product = this.ProductRepository.findById(item.getProduct().getId()).orElseThrow(() -> {
+           Product product = this.productRepository.findById(item.getProduct().getId()).orElseThrow(() -> {
                throw new RuntimeException("Produto não encontrado.");
            });
            
@@ -65,5 +75,45 @@ public class SaleService {
        sale = this.repository.save(sale);
        
        return this.mapper.toResponseDTO(sale);
+   }
+   
+   @Transactional
+   public SaleResponseDTO update(Long id, SaleRequestDTO request){
+       if(id < 1){
+           throw new RuntimeException("Id não pode ser menor que 1");
+       }
+       
+       Sale sale = this.repository.findById(id).orElseThrow(() -> {
+           throw new RuntimeException("Venda não encontrada.");
+       });
+       
+       this.mapper.updateEntityFromDTO(request, sale);
+       
+       Customer newCustomer = this.customerRepository.getReferenceById(request.customerId());
+       sale.setCustomer(newCustomer);
+       
+       User newUser = this.userRepository.getReferenceById(request.userId());
+       sale.setUser(newUser);
+       
+       for(SaleItem item : sale.getItems()){
+           Product product = this.productRepository.findById(item.getProduct().getId()).orElseThrow(() -> {
+               throw new RuntimeException("Produto não encontrado.");
+           });
+           
+           item.setProduct(product);
+           item.setUnitPrice(product.getPrice());
+           
+           BigDecimal quantity = BigDecimal.valueOf(item.getQuantity());
+           item.setSubTotal(product.getPrice().multiply(quantity));
+       }
+       
+        BigDecimal totalAmount = sale.getItems().stream()
+            .map(SaleItem::getSubTotal)
+            .reduce(BigDecimal.ZERO, BigDecimal::add);
+        sale.setTotalAmount(totalAmount);
+        
+        sale = this.repository.save(sale);
+        
+        return this.mapper.toResponseDTO(sale);
    }
 }
