7c300ff92f072d8544e07b1f2b855a7d3d294151
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-13 15:29:51 -0300

    feat: add toResponseDTOList, toItemResponseDTO, and toItemResponseDTOList methods to SaleMapper

--- backend/gestock/src/main/java/com/tiago/gestock/mapper/SaleMapper.java
+++ backend/gestock/src/main/java/com/tiago/gestock/mapper/SaleMapper.java
@@ -1,7 +1,10 @@
 package com.tiago.gestock.mapper;
 
 import com.tiago.gestock.model.Sale;
+import com.tiago.gestock.model.SaleItem;
+import com.tiago.gestock.model.dto.SaleItemResponseDTO;
 import com.tiago.gestock.model.dto.SaleResponseDTO;
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
@@ -11,5 +14,17 @@ public interface SaleMapper {
     
     @Mapping(source = "customer.name", target = "customerName")
     @Mapping(source = "user.name", target = "userName")
+    @Mapping(source = "customer.id", target = "customerId")
     SaleResponseDTO toResponseDTO(Sale entity);
+    
+    @Mapping(source = "customer.name", target = "customerName")
+    @Mapping(source = "user.name", target = "userName")
+    @Mapping(source = "customer.id", target = "customerId")
+    List<SaleResponseDTO> toResponseDTOList(List<Sale> entity);
+    
+    @Mapping(source = "product.id", target = "productId")
+    @Mapping(source = "product.name", target = "productName")
+    SaleItemResponseDTO toItemResponseDTO(SaleItem item);
+    
+    List<SaleItemResponseDTO> toItemResponseDTOList(List<SaleItem> items);
 }
