cc18d7b82e6b88537e75e788206f510185dfa639
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-13 13:39:49 -0300

    feat(product-controller): add create method

--- backend/gestock/src/main/java/com/tiago/gestock/controller/ProductController.java
+++ backend/gestock/src/main/java/com/tiago/gestock/controller/ProductController.java
@@ -1,14 +1,19 @@
 package com.tiago.gestock.controller;
 
+import com.tiago.gestock.model.dto.ProductRequestDTO;
 import com.tiago.gestock.model.dto.ProductResponseDTO;
 import com.tiago.gestock.service.ProductService;
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/products")
@@ -25,5 +30,15 @@ public class ProductController {
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponseDTO> findById(@PathVariable Long id){
         return ResponseEntity.ok(this.service.findById(id));
+    }
+    
+    @PostMapping()
+    public ResponseEntity<ProductResponseDTO> create(@RequestBody ProductRequestDTO request){
+        ProductResponseDTO response = this.service.create(request);
+        
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(response.id()).toUri();
+        
+        return ResponseEntity.created(uri).body(response);
     }
 }
