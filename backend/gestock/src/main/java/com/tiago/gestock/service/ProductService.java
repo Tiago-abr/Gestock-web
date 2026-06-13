ab3f197e75ac6ff1fda29014dd167191ef970034
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-13 13:38:53 -0300

    feat(product-service): add create method

--- backend/gestock/src/main/java/com/tiago/gestock/service/ProductService.java
+++ backend/gestock/src/main/java/com/tiago/gestock/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.tiago.gestock.service;
 
 import com.tiago.gestock.mapper.ProductMapper;
+import com.tiago.gestock.model.Product;
+import com.tiago.gestock.model.dto.ProductRequestDTO;
 import com.tiago.gestock.model.dto.ProductResponseDTO;
 import com.tiago.gestock.repository.ProductRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductService {
@@ -16,15 +19,24 @@ public class ProductService {
     @Autowired
     private ProductMapper mapper;
     
+    @Transactional(readOnly = true)
     public List<ProductResponseDTO> findAll(){
         return mapper.toResponseDTOList(this.repository.findAll());
     }
     
+    @Transactional(readOnly = true)
     public ProductResponseDTO findById(Long id){
         return mapper.toResponseDTO(
                 this.repository.findById(id).orElseThrow(() -> {
                     throw new RuntimeException("Produto não encontrado.");
                 })
         );
+    }
+    
+    @Transactional
+    public ProductResponseDTO create(ProductRequestDTO request){
+        Product product = this.mapper.toEntity(request);
+        product = this.repository.save(product);
+        return this.mapper.toResponseDTO(product);
     }
 }
