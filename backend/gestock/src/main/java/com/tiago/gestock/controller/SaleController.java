e69f76ffbf55d0df950f43aa1e9616c2b716976c
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-13 17:51:48 -0300

    feat(sale-controller): add create method

--- backend/gestock/src/main/java/com/tiago/gestock/controller/SaleController.java
+++ backend/gestock/src/main/java/com/tiago/gestock/controller/SaleController.java
@@ -1,13 +1,19 @@
 package com.tiago.gestock.controller;
 
+import com.tiago.gestock.model.dto.SaleRequestDTO;
 import com.tiago.gestock.model.dto.SaleResponseDTO;
 import com.tiago.gestock.service.SaleService;
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/sales")
@@ -19,5 +25,20 @@ public class SaleController {
     @GetMapping
     public ResponseEntity<List<SaleResponseDTO>> findAll(){
         return ResponseEntity.ok(this.service.findAll());
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<SaleResponseDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(this.service.findById(id));
+    }
+    
+    @PostMapping()
+    public ResponseEntity<SaleResponseDTO> create(@RequestBody SaleRequestDTO request){
+        SaleResponseDTO response = this.service.create(request);
+        
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(response.id()).toUri();
+        
+        return ResponseEntity.created(uri).body(response);
     }
 }
