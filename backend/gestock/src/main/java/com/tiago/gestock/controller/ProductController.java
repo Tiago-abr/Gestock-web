faea1f28c66e0bd4faaafbf746c0bd635ac6769c
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-13 13:50:22 -0300

    feat(product-controller): add update method

--- backend/gestock/src/main/java/com/tiago/gestock/controller/ProductController.java
+++ backend/gestock/src/main/java/com/tiago/gestock/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +41,10 @@ public class ProductController {
 				.buildAndExpand(response.id()).toUri();
         
         return ResponseEntity.created(uri).body(response);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> update(@PathVariable Long id, @RequestBody ProductRequestDTO request){
+        return ResponseEntity.ok(this.service.update(id, request));
     }
 }
