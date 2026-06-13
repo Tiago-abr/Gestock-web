88fc5bb2f03268cd9d88222f9e328b5d6e1b7b94
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-13 13:19:06 -0300

    feat(product-service): add findAll method

---
+++ backend/gestock/src/main/java/com/tiago/gestock/service/ProductService.java
@@ -0,0 +1,22 @@
+package com.tiago.gestock.service;
+
+import com.tiago.gestock.mapper.ProductMapper;
+import com.tiago.gestock.model.dto.ProductResponseDTO;
+import com.tiago.gestock.repository.ProductRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    
+    @Autowired
+    private ProductRepository repository;
+    
+    @Autowired
+    private ProductMapper mapper;
+    
+    public List<ProductResponseDTO> findAll(){
+        return mapper.toResponseDTOList(this.repository.findAll());
+    }
+}
