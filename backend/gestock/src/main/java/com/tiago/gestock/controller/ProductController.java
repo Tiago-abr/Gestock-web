9c8c9f17d6ec8d7bd2075a3708c05ce83b2b60b7
Tiago-abr <ti.abreu.da.rocha@gmail.com>
2026-06-14 17:08:08 -0300

    feat(controller): add CrossOrigin

--- backend/gestock/src/main/java/com/tiago/gestock/controller/ProductController.java
+++ backend/gestock/src/main/java/com/tiago/gestock/controller/ProductController.java
@@ -7,6 +7,7 @@ import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/products")
+@CrossOrigin(origins = "*")
 public class ProductController {
     
     @Autowired
